1f781d4d70f8323998778053e784f8deece232d5
Nicholas Nolen <nicnolen@ymail.com>
2022-01-14 08:38:05 -0600

    wrote failing test for getSchool() method

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,12 +1,16 @@
 // Import intern constructor function
 const Intern = require('../lib/Intern.js');
 
-// create the intern object
+// Create the intern object
 test('created the intern object', () => {
   const intern = new Intern('Nic', 25, 'nicnolen@ymail.com', 'UT Austin');
 
-  expect(intern.name).toEqual(expect.any(String));
-  expect(intern.id).toEqual(expect.any(Number));
-  expect(intern.email).toEqual(expect.any(String));
   expect(intern.school).toEqual(expect.any(String));
+});
+
+// Create a test for the getSchool method
+test('get intern school', () => {
+  const intern = new Intern('Nic', 25, 'nicnolen@ymail.com', 'UT Austin');
+
+  expect(intern.getSchool()).toEqual(expect.any(String));
 });
