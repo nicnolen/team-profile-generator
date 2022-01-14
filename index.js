1376172fb4329c7d4c183f5206a7eb7ff4da5b1b
Nicholas Nolen <nicnolen@ymail.com>
2022-01-14 11:09:09 -0600

    added validation to manager prompts

--- index.js
+++ index.js
@@ -7,34 +7,67 @@ const Manager = require('./lib/Manager');
 // Empty array to store added team members
 var teamMembersArr = [];
 
-// Prompts for the manager (should come first since only one manager)
+// Prompt users for manager information (should come first since only one manager)
 const addManager = () => {
   return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is the name of the team manager?',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      },
     },
 
     {
       type: 'input',
       name: 'id',
       message: "What is the team manager's employee ID?",
+      validate: idInput => {
+        if (idInput) {
+          return true;
+        } else {
+          console.log('Please enter a valid employee ID number!');
+          return false;
+        }
+      },
     },
 
     {
       type: 'input',
       name: 'email',
       message: "What is the team manager's email address?",
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else {
+          console.log('Please enter a valid email address!');
+          return false;
+        }
+      },
     },
 
     {
       type: 'input',
       name: 'officeNumber',
       message: "What is the team manager's office number?",
+      validate: officeNumberInput => {
+        if (officeNumberInput) {
+          return true;
+        } else {
+          console.log('Please enter a valid office number!');
+          return false;
+        }
+      },
     },
   ]);
 };
+// THEN make the new manager object
 addManager().then(managerInfo => {
   // define the managerInfo parameter as the name, id, email, and office number properties of the Manager constructor function
   const { name, id, email, officeNumber } = managerInfo;
@@ -43,6 +76,5 @@ addManager().then(managerInfo => {
 
   // push the new manager to the team array
   teamMembersArr.push(manager);
-  // check to see the new team members array
   console.log(teamMembersArr);
 });
