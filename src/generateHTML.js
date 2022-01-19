251d4b43327424e59744d2014fc9da2b5786e23e
Nicholas Nolen <nicnolen@ymail.com>
2022-01-19 08:31:50 -0600

    added intern card

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,3 +1,4 @@
+// Function to generate the team cards
 const generateCards = team => {
   // card array
   let cardArr = [];
@@ -61,16 +62,28 @@ const generateEngineer = function (engineer) {
         <h4>Engineer</h4>
       </div>
       <div class="card-body">
-      <p class="id">ID: ${engineer.id}</p>
-      <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
-      <p class="github">GitHub Username: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+        <p class="id">ID: ${engineer.id}</p>
+        <p class="email">Email: <a href="mailto:${engineer.email}">${engineer.email}</a></p>
+        <p class="github">GitHub Username: <a href="https://github.com/${engineer.github}">${engineer.github}</a></p>
+      </div>
+    </div>
     `;
 };
 
 // Generate Intern card
 const generateIntern = function (intern) {
   return `
-    <h3>${intern.name}</h3>
+    <div class="card">
+      <div class="card-header">
+        <h3>${intern.name}</h3>
+        <h4>Intern</h4>
+      </div>
+      <div class="card-body">
+        <p class="id">ID: ${intern.id}</p>
+        <p class="email">Email: <a href="mailto:${intern.email}">${intern.email}</a></p>
+        <p class="school">School: ${intern.school}</p>
+      </div>
+    </div>
     `;
 };
 
